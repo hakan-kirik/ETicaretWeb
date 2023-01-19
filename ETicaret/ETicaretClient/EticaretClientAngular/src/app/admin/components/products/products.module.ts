f0df0f9fb6427289cfaeab56815400bea38fecfd
HAKAN KIRIK <hakankirik7@gmail.com>
2023-01-19 21:47:48 +0300

    added-modal

--- ETicaret/ETicaretClient/EticaretClientAngular/src/app/admin/components/products/products.module.ts
+++ ETicaret/ETicaretClient/EticaretClientAngular/src/app/admin/components/products/products.module.ts
@@ -11,6 +11,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table'; 
 import {MatPaginatorModule} from '@angular/material/paginator'; 
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     CreateComponent,
     ListComponent,
     DeleteDirective,
+    DeleteDialogComponent,
   ],
   imports: [
     CommonModule,
@@ -29,7 +32,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule
   ]
 })
 export class ProductsModule { }
--- ETicaret/ETicaretClient/EticaretClientAngular/src/app/directives/admin/delete.directive.ts
+++ ETicaret/ETicaretClient/EticaretClientAngular/src/app/directives/admin/delete.directive.ts
@@ -1,47 +1,66 @@
 
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
-declare var $:any;
+declare var $: any;
 
 @Directive({
   selector: '[appDelete]'
 })
 export class DeleteDirective {
 
+
   constructor(
-    private element:ElementRef,
-    private _renderer:Renderer2,
-    private productService:ProductService,
-    private alertifyService:AlertifyService
-  ) 
-  { 
-    const img= _renderer.createElement("img");
-    img.setAttribute("src","../../../../../assets/delete_icon.png");
-    img.setAttribute("style","cursor:pointer;");
-    img.width=25;
-    img.height=25;
-    _renderer.appendChild(element.nativeElement,img);
+    private element: ElementRef,
+    private _renderer: Renderer2,
+    private productService: ProductService,
+    private alertifyService: AlertifyService,
+    public dialog: MatDialog
+  ) {
+    const img = _renderer.createElement("img");
+    img.setAttribute("src", "../../../../../assets/delete_icon.png");
+    img.setAttribute("style", "cursor:pointer;");
+    img.width = 25;
+    img.height = 25;
+    _renderer.appendChild(element.nativeElement, img);
   }
 
-  @Input() id:string;
-  @Output() callback:EventEmitter<any>=new EventEmitter();
+  @Input() id: string;
+  @Output() callback: EventEmitter<any> = new EventEmitter();
 
   @HostListener("click")
-  onClick(){
-  
-    const td:HTMLTableCellElement=this.element.nativeElement;
-    this.productService.delete(this.id);
-  
-    $(td.parentElement).fadeOut(2000);
-    this.callback.emit();
-    this.alertifyService.message("silindi",{
-      dismissOthers:true,
-      messageType:MessageType.Warning,
-      position:Position.TopRight
+  onClick() {
+    this.openDialog(async () => {
+      const td: HTMLTableCellElement = this.element.nativeElement;
+      await this.productService.delete(this.id);
 
-    })
+      $(td.parentElement).fadeOut(2000,()=>{
+        this.callback.emit();
+      });
+      
+      this.alertifyService.message("silindi", {
+        dismissOthers: true,
+        messageType: MessageType.Warning,
+        position: Position.TopRight
+
+      })
+    });
+
   }
 
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == DeleteState.Yes) {
+        afterClosed();
+      }
+    });
+
+  }
 }
